5673dbc5a5aa0510c627acdba32c57db09c8fc10
frogtube <u.pradere@gmail.com>
2017-11-13 15:51:51 +0100

    Adding links to species in list.html.twig and dynamic data

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -43,12 +43,15 @@ class GenusController extends Controller
     }
 
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
-        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
 
+        /*
         $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
         $key = md5($funFact);
 
@@ -60,10 +63,12 @@ class GenusController extends Controller
                 ->transform($funFact);
             $cache->save($key, $funFact);
         }
+        */
+        $this->get('logger')
+            ->info('Showing genus: ' .$genusName);
 
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName,
-            'funFact' => $funFact,
+            'genus' => $genus,
         ]);
     }
 
