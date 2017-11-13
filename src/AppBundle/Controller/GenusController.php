96673ce368090d320ecba88d7351d2f72969ce15
frogtube <u.pradere@gmail.com>
2017-11-13 08:50:37 +0100

    React JS added as JS notes display

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -16,23 +16,21 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-
         return $this->render('genus/show.html.twig', [
             'name' => $genusName,
-            'notes' => $notes
         ]);
     }
 
     /**
-     * @Route("/genus/{genusName}/notes")
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
      * @Method("GET")
      */
     public function getNotesAction()
     {
         $notes = [
-            'Octopus is slimmy',
-            'It has 8 legs',
-            'It is hell intelligent'
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
         ];
 
         $data = [
