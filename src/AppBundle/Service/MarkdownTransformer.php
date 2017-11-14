9731f4f06c5eedb16ead1074a8ea46bc895d74de
frogtube <u.pradere@gmail.com>
2017-11-14 12:59:25 +0100

    Adding parser function with a dependency injection

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -3,10 +3,21 @@
 namespace AppBundle\Service;
 
 
+
+use Knp\Bundle\MarkdownBundle\Parser\MarkdownParser;
+
 class MarkdownTransformer
 {
+    private $markDownParser;
+
+    public function __construct($markDownParser)
+    {
+        $this->markDownParser = new MarkdownParser();
+    }
+
     public function parse($str)
     {
-        return strtoupper($str);
+        return $this->markDownParser
+            ->transform($str);
     }
 }
