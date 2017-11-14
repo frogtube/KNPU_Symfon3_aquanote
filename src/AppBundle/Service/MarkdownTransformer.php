db278182875351d3cf53309a4eb47217cb73f4cf
frogtube <u.pradere@gmail.com>
2017-11-14 15:09:25 +0100

    Adding cache to the service

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -4,21 +4,33 @@ namespace AppBundle\Service;
 
 
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Knp\Bundle\MarkdownBundle\Parser\MarkdownParser;
 
 class MarkdownTransformer
 {
     private $markDownParser;
+    private $cache;
 
-    public function __construct(MarkdownParserInterface $markDownParser)
+    public function __construct(MarkdownParserInterface $markDownParser, Cache $cache)
     {
-        $this->markDownParser = new MarkdownParser();
+        $this->markDownParser = $markDownParser;
+        $this->cache = $cache;
     }
 
     public function parse($str)
     {
-        return $this->markDownParser
+        $cache = $this->cache;
+        $key = md5($str);
+        if ($cache->contains($key)) {
+            return $cache->fetch($key);
+        }
+
+        $str = $this->markDownParser
             ->transformMarkdown($str);
+        $cache->save($key, $str);
+
+        return $str;
     }
 }
